2c09bd1adeb0dd97e8b19f8b100b6c104a5ffd83
Gustavo Penha <guzpenha@Guz-notebook>
2017-05-10 11:32:33 -0300

    gráficos finais

--- tp1/src/vis_experiments.r
+++ tp1/src/vis_experiments.r
@@ -6,21 +6,33 @@ df$numHiddenUnits = as.character(df$numHiddenUnits)
 
 ggplot(df, aes(x=epoch,y=crossEntropy,color=numHiddenUnits)) + 
   geom_line() +
-  facet_grid(learningRate~gradientMethod)+ ggtitle("Empirical Error for each epoch")
+  scale_color_brewer(palette="Set1") + 
+  facet_grid(learningRate~gradientMethod)+ 
+  ggtitle("Empirical Error for each epoch")
+
+df = df[df$learningRate==0.01,]
+ggplot(df, aes(x=epoch,y=crossEntropy,color=numHiddenUnits)) + 
+  geom_line() + ylim(0,10)+
+  scale_color_brewer(palette="Set1") + 
+  facet_grid(learningRate~gradientMethod)+ 
+  ggtitle("Empirical Error for each epoch")
 
 df2 = read.csv("/home/guzpenha/personal/MachineLearningClass/tp1/data/experiments_results.csv",sep = ";")
 df2$numHiddenUnits = as.character(df2$numHiddenUnits)
 ggplot(df2, aes(x=updateIteration,y=crossEntropy,color=numHiddenUnits)) + 
   geom_line() +
+  scale_color_brewer(palette="Set1") +
   facet_grid(learningRate~gradientMethod) + ggtitle("Empirical Error for each weight update")
 
 df2 =df2[df2$learningRate==0.01,]
 ggplot(df2, aes(x=updateIteration,y=crossEntropy,color=numHiddenUnits)) + 
   geom_line() +
+  scale_color_brewer(palette="Set1") + ylim(0,10)+
   facet_grid(learningRate~gradientMethod) + ggtitle("Empirical Error")
 
 df3 = read.csv("/home/guzpenha/personal/MachineLearningClass/tp1/data/experiments_results_batch_size.csv",sep = ";")
 df3$batchSize = as.character(df3$batchSize)
 ggplot(df3, aes(x=epoch,y=crossEntropy,color=batchSize)) +
+  scale_color_brewer(palette="Set1") + 
  geom_line() + ggtitle("Empirical Error of Mini-Batch GD")
 
