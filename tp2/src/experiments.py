816082c5ee2c905575bc745c444e7060794c3a2e
Gustavo Penha <guzpenha@Guz-notebook>
2017-06-01 17:07:34 -0300

    modifying decisionStump class

--- tp2/src/experiments.py
+++ tp2/src/experiments.py
@@ -1,22 +1,29 @@
 import numpy as np
 import pandas as pd
-
+from sklearn.ensemble import AdaBoostClassifier
 from IPython import embed
 
 class DecisionStumpCategorical():
 	
 	def __init__(self):
-		pass
+		self.column_pred = None
 
-	def fit(self,stumpFeature,predValue,verbose=False):
-		self.stumpFeature = stumpFeature
-		self.predValue = predValue
-		
+	def fit(self,X,y,sample_weights):
+		# if(sample_weights== None):
+		# 	sample_weights = np.ones(X.shape[0])
+		best_error = 1
+		best_h_column = None		
+		for c in range(X.shape[1]):
+			X[X[:,c] == 0,c] = -1
+			error = (X[:,c] != y).dot(sample_weights)			
+			if(error < best_error):
+				best_error = error
+				best_h_column = c
+		self.column_pred = best_h_column		
 		return self
 
 	def predict(self,X):
-		y = pd.DataFrame(X).apply(lambda r,v=self.predValue,f=self.stumpFeature: 1 if str(r[f])==v else -1,axis=1)
-		return y
+		return X[:,self.column_pred]
 
 class AdaBoostDecisionStump():
 	
@@ -25,42 +32,27 @@ class AdaBoostDecisionStump():
 		self.hypothesis = []
 		self.hypothesis_weights = []
 		self.n_estimators = n_estimators
-
-	def best_single_stump(self,X,y):
-		min_error_sum = 1000
-		min_error = []
-		h = None
-		for feature in range(X.shape[1]):
-			for value in np.unique(X[:,feature]):				
-				dsc = DecisionStumpCategorical()
-				h = dsc.fit(feature,value)
-				pred = h.predict(X)
-				error = (pred != y).dot(self.sample_weights)
-				if(error.mean()<min_error_sum):
-					min_error_sum = error.mean()
-					min_error = error
-					min_h = h
-		return min_h, min_error
 		
 	def fit(self,X,y,verbose=False):		
 		#All instances have equal initial weight
 		self.sample_weights = np.ones(X.shape[0])/X.shape[0]
 
 		for i in range(self.n_estimators):
-			h, error = self.best_single_stump(X,y)					
-			alpha = 0.5 * (np.log(1 - error.mean()) - np.log(error.mean())) # calculate alpha
+			dsc = DecisionStumpCategorical()
+			h = dsc.fit(X,y,sample_weights=self.sample_weights)
+			error = (h.predict(X) != y).dot(self.sample_weights)
+			alpha = 0.5 * (np.log(1 - error) - np.log(error)) # calculate alpha
 
 			self.hypothesis.append(h)
 			self.hypothesis_weights.append(alpha)
 
 			self.sample_weights = self.sample_weights * np.exp(-alpha * h.predict(X) * y)
-			self.sample_weights = self.sample_weights/self.sample_weights.sum()
-	
+			self.sample_weights = self.sample_weights/self.sample_weights.sum()				
+
 	def predict(self,X):
 		pred = np.zeros(X.shape[0])
 		for (h, alpha) in zip(self.hypothesis,self.hypothesis_weights):
-			pred+= h.predict(X) * alpha
-		# embed()
+			pred+= h.predict(X) * alpha	
 		return np.sign(pred)
 
 def main():	
@@ -69,15 +61,16 @@ def main():
 	
 	#Preprocess data	
 	data["label"] = data.apply(lambda r: 1 if r.label == "positive" else -1,axis=1)
-	X = data[[c for c in data.columns if c !="label"]].as_matrix()	
+	X = pd.get_dummies(data[[c for c in data.columns if c !="label"]]).as_matrix()	
 	y = data["label"]
-
+	
 	# Fit the data using the AdaBoostDecisionStump
-	bds = AdaBoostDecisionStump(n_estimators=20)
-	bds.fit(X,y,verbose=True)
-	pred = bds.predict(X)
-	print((pred==y).sum())
-	print(len(pred))
+	for n_est in range(1,10):
+		bds = AdaBoostDecisionStump(n_estimators=n_est)
+		# bds = AdaBoostClassifier(n_estimators=n_est)
+		bds.fit(X,y)
+		pred = bds.predict(X)
+		print("Acuracia: " +str((pred==y).sum()/float(len(pred))))
 	# embed()
 
 if __name__ == "__main__":
