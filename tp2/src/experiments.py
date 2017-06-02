f5a9c6d81c177ad122d56188e46f839526ccbf50
guz <guzpenha10@gmail.com>
2017-06-01 21:46:07 -0300

    Fixing stump by adding constant true, constante false and negative of columns

--- tp2/src/experiments.py
+++ tp2/src/experiments.py
@@ -8,37 +8,92 @@ from scipy.stats import mode
 class DecisionStumpCategorical():
 	
 	def __init__(self):
-		self.column_pred = None			
-		self.predictions = []
+		self.column_pred = None
+		self.inverted = None
+		self.constant = False
 
-	def fit(self,X,y,sample_weight):
-		self.y = y		
+	def fit(self,X_input,y,sample_weight):
+		X = X_input.copy()
 		best_error = float("inf")
+		best_h_column = None		
 
 		for c in range(X.shape[1]):
-			cut_zeros = X[:,c] == 0
-			cut_ones = X[:,c] == 1
-			mode_0 = mode(y[cut_zeros])[0][0]
-			mode_1 = mode(y[cut_ones])[0][0]
-
-			pred = np.ones(X.shape[0])
-			pred[cut_zeros] = mode_0
-			pred[cut_ones] = mode_1
-			pred_map = (mode_0,mode_1)			
-			error = (pred != y).dot(sample_weight)			
-
+			X[X[:,c] == 0,c] = -1
+			error = (X[:,c] != y).dot(sample_weight)			
 			if(error < best_error):
 				best_error = error
-				self.column_pred = c				
-				self.predictions = pred_map
+				best_h_column = c
+				self.inverted = False
 
+		for c in range(X.shape[1]):						
+			X[X[:,c] == 1,c] = -1
+			X[X[:,c] == 0,c] =  1
+			error = (X[:,c] != y).dot(sample_weight)			
+			if(error < best_error):
+				best_error = error
+				best_h_column = c
+				self.inverted = True
+
+		error = (np.ones(X.shape[0]) != y).dot(sample_weight)
+		if(error < best_error):
+			self.constant = 1
+		error = (np.ones(X.shape[0])*-1 != y).dot(sample_weight)
+		if(error < best_error):
+			self.constant = -1
+		
+
+		self.column_pred = best_h_column
 		return self
 
-	def predict(self,X):
-		pred = np.ones(X.shape[0])
-		pred[X[:,self.column_pred] == 0] = self.predictions[0]
-		pred[X[:,self.column_pred] == 1] = self.predictions[1]
-		return pred
+	def predict(self,X_input):
+		X = X_input.copy()
+		if(self.constant == 1):
+			return np.ones(X.shape[0])
+		elif(self.constant == -1):
+			return np.ones(X.shape[0]) *-1			
+
+		if(self.inverted):
+			X[X[:,self.column_pred] == 1,self.column_pred] = -1
+			X[X[:,self.column_pred] == 0,self.column_pred] =  1
+		else:
+			X[X[:,self.column_pred] == 0,self.column_pred] = -1
+		return X[:,self.column_pred]
+
+
+# class DecisionStumpCategoricalTree():
+	
+# 	def __init__(self):
+# 		self.column_pred = None			
+# 		self.predictions = []
+
+# 	def fit(self,X,y,sample_weight):
+# 		self.y = y		
+# 		best_error = float("inf")
+
+# 		for c in range(X.shape[1]):
+# 			cut_zeros = X[:,c] == 0
+# 			cut_ones = X[:,c] == 1
+# 			mode_0 = mode(y[cut_zeros])[0][0]
+# 			mode_1 = mode(y[cut_ones])[0][0]
+
+# 			pred = np.ones(X.shape[0])
+# 			pred[cut_zeros] = mode_0
+# 			pred[cut_ones] = mode_1
+# 			pred_map = (mode_0,mode_1)			
+# 			error = (pred != y).dot(sample_weight)			
+
+# 			if(error < best_error):
+# 				best_error = error
+# 				self.column_pred = c				
+# 				self.predictions = pred_map
+
+# 		return self
+
+# 	def predict(self,X):
+# 		pred = np.ones(X.shape[0])
+# 		pred[X[:,self.column_pred] == 0] = self.predictions[0]
+# 		pred[X[:,self.column_pred] == 1] = self.predictions[1]
+# 		return pred
 
 class AdaBoostDecisionStump():
 	
@@ -56,6 +111,7 @@ class AdaBoostDecisionStump():
 			dsc = DecisionStumpCategorical()
 			# dsc = DecisionTreeClassifier(max_depth=1)
 			h = dsc.fit(X,y,sample_weight=self.sample_weights)
+			# print(h.predict(X))
 			error = (h.predict(X) != y).dot(self.sample_weights)
 			alpha = 0.5 * (np.log((1 - error)/error)) # calculate alpha
 
@@ -63,7 +119,7 @@ class AdaBoostDecisionStump():
 			self.hypothesis_weights.append(alpha)
 
 			self.sample_weights = self.sample_weights * np.exp(-alpha * h.predict(X) * y)
-			self.sample_weights = (self.sample_weights/self.sample_weights.sum()).as_matrix()
+			self.sample_weights = (self.sample_weights/self.sample_weights.sum()).as_matrix()			
 
 	def predict(self,X):
 		pred = np.zeros(X.shape[0])
@@ -77,13 +133,12 @@ def main():
 	
 	#Preprocess data	
 	data["label"] = data.apply(lambda r: 1 if r.label == "positive" else -1,axis=1)
-	X = pd.get_dummies(data[[c for c in data.columns if c !="label"]]).as_matrix()	
-	y = data["label"]
-	
+	X = pd.get_dummies(data[[c for c in data.columns if c !="label"]]).as_matrix().astype(int)
+	y = data["label"]	
 	# Fit the data using the AdaBoostDecisionStump
-	for n_est in range(1,20):
-		bds = AdaBoostDecisionStump(n_estimators=n_est)
-		# bds = AdaBoostClassifier(n_estimators=n_est)
+	for n_est in [1000]:#range(1,1000):
+		# bds = AdaBoostDecisionStump(n_estimators=n_est)
+		bds = AdaBoostClassifier(n_estimators=n_est)
 		bds.fit(X,y)
 		pred = bds.predict(X)
 		print("Acuracia: " +str((pred==y).sum()/float(len(pred))))
